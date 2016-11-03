468c276bd145e48b06c300876bb108157c67f1bb
KhalladB <eng.khallad@hotmail.com>
2016-11-03 13:48:40 +0300

    Update openag_dfr0161.h

--- openag_dfr0161.h
+++ openag_dfr0161.h
@@ -40,7 +40,7 @@ class Dfr0161: public Module {
   // From dfr0161_0300_sensor
   String _ph_instruction_code;
   int _ph_instruction_id;
-  float _ph_calibratoin_coefficient;
+  float _ph_calibration_coefficient;
   float _ph_calibration_offset;
   
   
