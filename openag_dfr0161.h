a527ede3f2ff90b68874d8984f6cf947b13f67d9
KhalladB <eng.khallad@hotmail.com>
2016-11-03 13:17:52 +0300

    Update openag_dfr0161.h

--- openag_dfr0161.h
+++ openag_dfr0161.h
@@ -7,6 +7,7 @@
 #include "WProgram.h"
 #endif
 
+#include <OneWire.h>
 #include <openag_module.h>
 #include <std_msgs/Float32.h>
 
@@ -18,16 +19,30 @@ class Dfr0161: public Module{
   void update():
   boot get_water_potential_hydrogen(std_msgs::Float32 &msg);
   
+  // From dfr0161_0300_sensor
+  float averageArray(int* arr, int number);
+  
+  // From dfr0161_0300_sensor
+  float _ph_raw;
+  float _ph_filtered;
+  
   private:
   void getData();
   bool readSensor();
   
   // Private Variables
-  int _pin;
+  int _ph_pin;
   float _water_potential_hydrogen;
   bool _send_water_potential_hydrogen;
   unite32_t _time_of_last_reading;
   const unit32_t _min_update_interval = 1800;
+  
+  // From dfr0161_0300_sensor
+  string _ph_instruction_code;
+  int _ph_instruction_id;
+  float _ph_calibratoin_coefficent;
+  float _ph_calibration_offset;
+  
   
 
 
